175dfa22530d17e8c06537768e1040afe82e5af0
Johannes Doerfert <jdoerfert.llvm@gmail.com>
2026-05-01 16:16:45 -0700

    Apply suggestion from @jdoerfert

--- clang/test/Instrumentor/StackUsageRT.cpp
+++ clang/test/Instrumentor/StackUsageRT.cpp
@@ -1,4 +1,4 @@
-//===-- examples/Instrumentor/stack_usage.c - An example Instrumentor use -===//
+//===-- test/Instrumentor/StackUsageRT.cpp - An example Instrumentor use -===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
